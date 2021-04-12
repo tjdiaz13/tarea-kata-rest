b71d1867297262c3ecce37e01b33fbe6c73039b1
tdiaz1 <tj.diaz@uniandes.edu.co>
2021-04-11 23:13:33 -0500

    initial config - create model

--- agileProject/gallery/admin.py
+++ agileProject/gallery/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Portafolio
+from .models import Portfolio
 
 # Register your models here.
-admin.site.register(Portafolio)
+admin.site.register(Portfolio)
--- agileProject/gallery/models.py
+++ agileProject/gallery/models.py
@@ -3,10 +3,9 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Portafolio(models.Model):
-    producto = models.CharField(max_length=300)
-    usuario_registrado = models.ForeignKey(User, on_delete=models.CASCADE)
-    publico = models.BooleanField()
+class Portfolio(models.Model):
+    product = models.CharField(max_length=300)
+    user = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
+    public = models.BooleanField()
 
-    def __str__(self):
-        return 'Portafolio: ' + self.producto
+
