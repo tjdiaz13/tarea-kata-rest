392c4eec112c3005b88033f304224fe3b3872a77
tdiaz1 <tj.diaz@uniandes.edu.co>
2021-04-11 23:17:27 -0500

    Status de respuesta de lista de portafolios (ROJO)

--- agileProject/gallery/tests.py
+++ agileProject/gallery/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class PortfolioTestCase(TestCase):
+
+    def test_list_portfolio_status(self):
+        url = '/gallery/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 200)
