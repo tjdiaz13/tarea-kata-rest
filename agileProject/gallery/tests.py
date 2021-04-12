a59f1cb8e53f688944df742b655d43f35cf1c5d8
tdiaz1 <tj.diaz@uniandes.edu.co>
2021-04-11 23:24:20 -0500

    PASO 2 - Cantidad de elementos de la lista de portafolio (ROJO)

--- agileProject/gallery/tests.py
+++ agileProject/gallery/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Portfolio
+import json
 
 
 # Create your tests here.
@@ -8,3 +11,12 @@ class PortfolioTestCase(TestCase):
         url = '/gallery/'
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, 200)
+
+    def test_count_portfolio_list(self):
+        user_model = User.objects.create_user(username='test', password='pass', first_name='test', last_name='test',
+                                              email='email')
+        Portfolio.objects.create(product='test', user=user_model, public=True)
+        Portfolio.objects.create(product='test', user=user_model, public=False)
+        response = self.client.get('/gallery/')
+        current_data = json.loads(response.content)
+        self.assertEqual(len(current_data), 2)
