1529382ce352d225481ce7f3bf5b270c448c25a2
tdiaz1 <tj.diaz@uniandes.edu.co>
2021-04-11 23:22:16 -0500

    PASO 1 - Status de respuesta de lista de portafolios (VERDE)

--- agileProject/gallery/views.py
+++ agileProject/gallery/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+from .models import Portfolio
+import json
 
 # Create your views here.
+@csrf_exempt
+def index(request):
+    portfolio_list = []
+    return HttpResponse(serializers.serialize('json', portfolio_list))
